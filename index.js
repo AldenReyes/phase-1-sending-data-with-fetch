5a26f810a2eda3be38fa2368ad0db0b774927976
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-04-23 17:39:34 -0700

    Completed Lab; Using fetch requests to POST data to a server and then updating the DOM with the server response

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+  .then (resp => resp.json())
+  .then (data => appendIDToDOM(data))
+  .catch(data => sendErrorToDOM(data))
+};
+
+function appendIDToDOM(obj) {
+  const body = document.querySelector('body');
+  body.innerHTML = JSON.stringify(obj.id);
+}
+
+function sendErrorToDOM(obj) {
+  const body = document.querySelector('body');
+  body.innerHTML = JSON.stringify(obj.message);
+}
